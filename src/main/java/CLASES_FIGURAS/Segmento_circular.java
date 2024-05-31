4b7a5e0aa337dfe91c665a4bf2070e4446e41a01
1-21-01-E043-20\POO <joahn.cardenas@epn.edu.ec>
2024-05-31 12:44:35 -0500

    circulo

--- src/main/java/CLASES_FIGURAS/Segmento_circular.java
+++ src/main/java/CLASES_FIGURAS/Segmento_circular.java
@@ -4,5 +4,31 @@ public class Segmento_circular extends  Circulo{
      double radio;
      double grados;
 
-     //Construdores
-}
+     //Constructor
+
+     public Segmento_circular() {
+     }
+
+     //Constructor
+
+     public Segmento_circular(double radio, double grados) {
+          this.radio = radio;
+          this.grados = grados;
+     }
+
+     //setters, getters
+     public double getRadio() {
+          return radio;
+     }
+     public void setRadio(double radio) {
+          this.radio = radio;
+     }
+     public double getGrados() {
+          return grados;
+     }
+     public void setGrados(double grados) {
+          this.grados = grados;
+     }
+
+
+     }
