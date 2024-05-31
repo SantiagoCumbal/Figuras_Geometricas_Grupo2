f78bd8b91549ca4b9c5985e2a4912a063f94a952
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-05-31 12:04:36 -0500

    ajuste 3d

--- src/main/java/CLASES_FIGURAS/TRES_D.java
+++ src/main/java/CLASES_FIGURAS/TRES_D.java
@@ -3,4 +3,25 @@ package CLASES_FIGURAS;
 public class TRES_D {
     int ncaras;
     double aristas;
+    public TRES_D() {}
+    public TRES_D(int ncaras, double aristas) {
+        this.ncaras = ncaras;
+        this.aristas = aristas;
+    }
+
+    public int getNcaras() {
+        return ncaras;
+    }
+
+    public void setNcaras(int ncaras) {
+        this.ncaras = ncaras;
+    }
+
+    public double getAristas() {
+        return aristas;
+    }
+
+    public void setAristas(double aristas) {
+        this.aristas = aristas;
+    }
 }
