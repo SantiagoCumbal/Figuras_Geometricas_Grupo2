88b909926cd9d89c5e293ae3885e9921551e487d
1-21-01-E043-28\POO <john.cobacango@epn.edu.ec>
2024-05-31 12:45:59 -0500

    area  de trangulo y circulo

--- src/main/java/CLASES_FIGURAS/Circulo.java
+++ src/main/java/CLASES_FIGURAS/Circulo.java
@@ -2,5 +2,33 @@ package CLASES_FIGURAS;
 
 public class Circulo extends regulares {
     double radio;
+    double diametro;
 
+    public Circulo() {
+    }
+
+    public Circulo(double radio, double diametro) {
+        this.radio = radio;
+        this.diametro = diametro;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(double diametro) {
+        this.diametro = diametro;
+    }
+
+    public double area(double radio) {
+        return Math.PI * Math.pow(radio, 2);
+    }
 }
--- src/main/java/CLASES_FIGURAS/Triangulo.java
+++ src/main/java/CLASES_FIGURAS/Triangulo.java
@@ -3,4 +3,36 @@ package CLASES_FIGURAS;
 public class Triangulo extends regulares{
     double base;
     double altura;
+
+    public Triangulo() {
+    }
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double area(double base, double altura) {
+        return (base*altura)/2;
+    }
+
+    public void imprimir_datos(){
+        System.out.println(area);
+    }
 }
