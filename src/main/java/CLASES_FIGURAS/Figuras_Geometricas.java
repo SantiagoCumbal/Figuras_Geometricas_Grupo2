472816a687376d535d14d69757179c9e12698d5c
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-05-31 12:01:11 -0500

    clases

--- src/main/java/CLASES_FIGURAS/Figuras_Geometricas.java
+++ src/main/java/CLASES_FIGURAS/Figuras_Geometricas.java
@@ -2,4 +2,5 @@ package CLASES_FIGURAS;
 
 public class Figuras_Geometricas {
     int nlados;
+    String nombre;
 }
---
+++ src/main/java/CLASES_FIGURAS/TRES_D.java
@@ -0,0 +1,6 @@
+package CLASES_FIGURAS;
+
+public class TRES_D {
+    int ncaras;
+    double aristas;
+}
