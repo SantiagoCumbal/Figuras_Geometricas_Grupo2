3c57f22b1a1ded939c79c84a41adbd31db2ab9aa
donruffo-admin <dennisdiaz407@gmail.com>
2024-05-31 12:16:26 -0500

    CONSTRUCTORES Y METODOS DOS_D

--- src/main/java/CLASES_FIGURAS/DOS_D.java
+++ src/main/java/CLASES_FIGURAS/DOS_D.java
@@ -2,5 +2,33 @@ package CLASES_FIGURAS;
 
 public class DOS_D extends Figuras_Geometricas {
     double area, perimetro;
+    public DOS_D(){}
+    public DOS_D(double area, double perimetro,int nlados, String nombre) {
+        super(nlados, nombre);
+        this.area = area;
+        this.perimetro = perimetro;
+    }
 
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public double calcularArea() {
+        return area * perimetro;
+    }
+    public double calcularPerimetro() {
+        return perimetro * nlados;
+    }
 }
