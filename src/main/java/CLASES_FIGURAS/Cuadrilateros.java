04a8af872ba9ddfcb4b59728867e3a0e9f85e9e9
DESKTOP-SHSRL6N\Luxyz <elkin.diaz@epn.edu.ec>
2024-06-04 21:53:37 -0500

    Cambio Elkin Diaz

--- src/main/java/CLASES_FIGURAS/Cuadrilateros.java
+++ src/main/java/CLASES_FIGURAS/Cuadrilateros.java
@@ -1,6 +1,31 @@
 package CLASES_FIGURAS;
 
-public class Cuadrilateros {
-    double area;
-    double perimetro;
+public class Cuadrilateros extends regulares{
+    int vertices;
+    double medidaLados;
+
+    public Cuadrilateros() {
+    }
+
+    public Cuadrilateros(double area, double perimetro, int nlados, String nombre, double altura, double base, int vertices, double medidaLados) {
+        super(area, perimetro, nlados, nombre, altura, base);
+        this.vertices = vertices;
+        this.medidaLados = medidaLados;
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(int vertices) {
+        this.vertices = vertices;
+    }
+
+    public double getMedidaLados() {
+        return medidaLados;
+    }
+
+    public void setMedidaLados(double medidaLados) {
+        this.medidaLados = medidaLados;
+    }
 }
--- src/main/java/CLASES_FIGURAS/regulares.java
+++ src/main/java/CLASES_FIGURAS/regulares.java
@@ -3,4 +3,41 @@ package CLASES_FIGURAS;
 public class regulares extends DOS_D  {
 double base;
 double altura;
+
+    public regulares() {
+    }
+
+    public regulares(double area, double perimetro, int nlados, String nombre, double altura, double base) {
+        super(area, perimetro, nlados, nombre);
+        this.altura = altura;
+        this.base = base;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        System.out.println("Area del "+getNombre());
+        return super.calcularArea();
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        System.out.println("Perimetro del "+getNombre());
+        return super.calcularPerimetro();
+    }
 }
